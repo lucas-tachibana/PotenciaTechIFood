05aa787aef7f13ce73106c40bebc2f89c28e9ddf
lucas-tachibana <lucas_tachibana@hotmail.com>
2022-10-07 18:16:33 -0300

    exercício da aula 3

--- aprendendo-sintaxe-java/java-anatomia-classes/src/MinhaClasse.java
+++ aprendendo-sintaxe-java/java-anatomia-classes/src/MinhaClasse.java
@@ -2,27 +2,15 @@ import java.sql.Date;
 
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("OOOOOOOOOi :)");
-        String nome = "Lucas";
-        int anoNascimento = 1989;
-        int anoAtual = 1995;
-        boolean maiorIdade;
-        String maiorOuMenor;
+        String primeiroNome = "Lucas";
+        String segundoNome = "Tachibana";
 
-        maiorIdade = verificaIdade(anoNascimento, anoAtual);
-        if (maiorIdade) {
-            maiorOuMenor = "maior de idade.";
-        } else {
-            maiorOuMenor = "menor de idade.";
-        }
-        System.out
-                .println(nome + " nasceu em " + anoNascimento + ", portanto em " + anoAtual + " ele é " + maiorOuMenor);
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
 
+        System.out.println(nomeCompleto);
     }
 
-    private static boolean verificaIdade(int anoNascimento, int anoAtual) {
-        if (anoAtual - anoNascimento > 18)
-            return true;
-        return false;
+    private static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do Método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
