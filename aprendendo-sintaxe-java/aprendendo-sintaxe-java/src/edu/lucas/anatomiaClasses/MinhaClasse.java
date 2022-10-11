7b3288539e18c672a6e3a4d03d80aa9297b52b3a
lucas-tachibana <lucas_tachibana@hotmail.com>
2022-10-11 11:04:20 -0300

    Alteração de nome do projeto

---
+++ aprendendo-sintaxe-java/aprendendo-sintaxe-java/src/edu/lucas/anatomiaClasses/MinhaClasse.java
@@ -0,0 +1,16 @@
+package edu.lucas.anatomiaClasses;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Lucas";
+        String segundoNome = "Tachibana";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    private static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do Método: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
