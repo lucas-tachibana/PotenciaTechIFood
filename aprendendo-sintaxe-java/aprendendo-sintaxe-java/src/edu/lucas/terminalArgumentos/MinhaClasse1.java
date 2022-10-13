cac2fea14ba34cc606f5e3a1216551f0d89335e7
lucas-tachibana <lucas_tachibana@hotmail.com>
2022-10-13 11:46:21 -0300

    Adiciona exercício de terminal

---
+++ aprendendo-sintaxe-java/aprendendo-sintaxe-java/src/edu/lucas/terminalArgumentos/MinhaClasse1.java
@@ -0,0 +1,7 @@
+package edu.lucas.terminalArgumentos;
+
+public class MinhaClasse1 {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
